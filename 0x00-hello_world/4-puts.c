4a0a968acbdf924b5e02ed6dead1a980dfdfb0bc
ChristianDzanka <christiandzanka@gmail.com>
2023-03-16 17:22:46 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * This is my first c programme
+ * main - will print to standard out put using puts
+ * when RETURN: (0) it means my programme is successful
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
