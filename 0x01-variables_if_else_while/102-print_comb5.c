083907dc5edddc0ff33cc73d5749b87172efe109
ChristianDzanka <christiandzanka@gmail.com>
2023-03-17 19:27:41 +0000

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int w, e;
+
+	for (w = 0; w <= 99; w++)
+	{
+		for (e = w + 1; e <= 99; e++)
+		{
+			putchar((w / 10) + '0');
+			putchar((w % 10) + '0');
+			putchar(' ');
+			putchar((e / 10) + '0');
+			putchar((e / 10) + '0');
+				if (w == 98 && e == 99)
+				{
+					continue;
+				}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
