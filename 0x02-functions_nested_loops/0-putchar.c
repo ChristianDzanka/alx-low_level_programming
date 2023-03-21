06d4590a8b05cba5cedc5e6529626fd5c7951a02
ChristianDzanka <christiandzanka@gmail.com>
2023-03-21 11:56:55 +0000

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char fte[] = "_putchar\n";
+int i;
+
+while (fte[i] != '\0')
+{
+putchar(fte[i]);
+i++;
+}
+return (0);
+}
