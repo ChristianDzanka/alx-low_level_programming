b03270371a7d84216ce08d5e9842a682cb4cce90
ChristianDzanka <christiandzanka@gmail.com>
2023-03-23 17:46:27 +0000

    8-print_square.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,4 +9,5 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+void print_square(int size);
 #endif
