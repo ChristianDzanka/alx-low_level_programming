4a8678470672bb53895cd56d0b1477a84215d52f
ChristianDzanka <christiandzanka@gmail.com>
2023-03-27 15:08:51 +0000

    Understanding

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+void _puts(char *str);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _putchar(char c);
