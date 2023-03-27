9e886b819dfc68653817b1290524737949e5c30b
ChristianDzanka <christiandzanka@gmail.com>
2023-03-27 18:10:40 +0000

    Advance

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,54 +2,46 @@
 
 /**
  * _atoi - converts a string to an integer
- * @str: string to be converted
+ * @s: string to be converted
  *
  * Return: the int converted from the string
  */
-int _atoi(char *str)
+int _atoi(char *s)
 {
-	int index = 0;
-	int negative_signs_count = 0;
-	int integer_value = 0;
-	int string_length = 0;
-	int integer_parsed = 0;
-	int current_digit = 0;
+	int i, d, n, len, f, digit;
 
-	/* get the length of the string */
-	while (str[string_length] != '\0')
-		string_length++;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	/* process the string one character at a time */
-	while (index < string_length && integer_parsed == 0)
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		/* check if the current character is a negative sign */
-		if (str[index] == '-')
-			negative_signs_count++;
+		if (s[i] == '-')
+			++d;
 
-		/* check if the current character is a digit */
-		if (str[index] >= '0' && str[index] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			/* parse the digit and handle negative signs */
-			current_digit = str[index] - '0';
-			if (negative_signs_count % 2 != 0)
-				current_digit = -current_digit;
-			integer_value = integer_value * 10 + current_digit;
-
-			/* indicate that at least one digit was parsed */
-			integer_parsed = 1;
-
-			/* check if there are no more digits to parse */
-			if (str[index + 1] < '0' || str[index + 1] > '9')
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			/* reset the integer_parsed flag if there are more digits */
-			integer_parsed = 0;
+			f = 0;
 		}
-		index++;
+		i++;
 	}
 
-	/* if no digit was parsed, return 0 */
-	if (integer_parsed == 0)
+	if (f == 0)
 		return (0);
 
-	return (integer_value);
+	return (n);
 }
+
