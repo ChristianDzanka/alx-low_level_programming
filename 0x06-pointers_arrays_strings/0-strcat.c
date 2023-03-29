b79a3c61ae32a76a4819941e253a382b362fade8
ChristianDzanka <christiandzanka@gmail.com>
2023-03-29 15:58:51 +0000

    3rd

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a, b;
+
+	a = 0;
+	b = 0;
+
+	/* to reach the end of the dest before appending src */
+	while (dest[a] != '\0')
+		a++;	/* now dest is at the end of its string */
+
+	/* now to append src to the end of dest */
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+	/* add the null character to dest */
+	dest{i} = '\0';
+
+	/* return the appended string */
+	return (dest);
+}
